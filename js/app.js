2ec383a9a871ffad2122b6924b0d7603a502ea8d
Lapo0 <lapo.gini.01@gmail.com>
2023-02-13 18:14:11 +0100

    REFACTOR funcion

--- js/app.js
+++ js/app.js
@@ -8,31 +8,28 @@ console.log(numeriRandom)
 // Creare alert per mostrare numeri
 alert('Ricorda questi numeri: ' + numeriRandom)
 
-let numeriDaRicordare = []
 
 // Assegnare SetTimeout per richiamare la funzione
 setTimeout(function() {
-    isRememberNumber(numeriDaRicordare);
+    let randomNumbersPc = numbersToRemeber();
 
     const numeriCombacianti = []
 
     for (let i = 0; i < numeriRandom.length; i++) {
-        for ( let j = 0; j < numeriDaRicordare.length; j++) {
-            if ( numeriRandom[i] === numeriDaRicordare[j]) {
+        for ( let j = 0; j < randomNumbersPc.length; j++) {
+            if ( numeriRandom[i] === randomNumbersPc[j]) {
                 numeriCombacianti.push(numeriRandom[i])
             }
         }
     }
 
     console.log('Hai azzeccato: ' + numeriCombacianti)
-    console.log('Hai azzeccato: ' + numeriCombacianti.length)
+    console.log('Hai azzeccato: ' + numeriCombacianti.length + ' numeri')
 }, 3000);
 
 
 
-
-
-//////////////// FUNZIONI
+//////////////// FUNZIONI //////////////////////
 
 function isRandomNumber() {
     while (numeriRandom.length < 5) {
@@ -44,19 +41,18 @@ function isRandomNumber() {
     return numeriRandom
 }
 
-
-function isRememberNumber(numeri) {
-    
+function numbersToRemeber() {
+    let randomNumbersPc = [];
     for (let i = 0; i < 5; i++) {
-        let numeroDaRicordare
+        let numeroDaRicordare;
         while (isNaN(numeroDaRicordare)) {
-            numeroDaRicordare = parseInt(prompt('Scrivi i numeri che ricordi'))
+            numeroDaRicordare = parseInt(prompt('Scrivi i numeri che ricordi'));
             if (isNaN(numeroDaRicordare)) {
-                alert('Inserisci un numero valido')
+                alert('Inserisci un numero valido');
             }
         }
-        numeri.push(numeroDaRicordare)
+        randomNumbersPc.push(numeroDaRicordare);
     }
-    console.log(numeri)
+    console.log(randomNumbersPc)
+    return randomNumbersPc;
 }
-
