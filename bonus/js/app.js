61dece3eca0a4e22a068883162904864f786182c
Lapo0 <lapo.gini.01@gmail.com>
2023-02-13 21:46:06 +0100

    FEAT style numeri random.innerHTML

--- bonus/js/app.js
+++ bonus/js/app.js
@@ -1,58 +1,76 @@
 // Richiamare il pulsante
 const play = document.getElementById('play')
 
-// Creare numeri random
-let numeriRandom = [];
+const griglia = document.getElementById('griglia')
 
-// Creare variabile per calcolare 30 secondi
-const sec = isAvailableTime()
 
 play.addEventListener('click', function() {
-
-    isRandomNumber(numeriRandom)
+    // Creare numeri random
+    let numeriRandom = isRandomNumber()
     console.log(numeriRandom)
+    
+    // Creare variabile per calcolare 30 secondi
+    const sec = isAvailableTime()
 
-    // Creare alert per mostrare numeri
-    alert('Ricorda questi numeri: ' + numeriRandom)
+    // Creare ciclo per creare div
+    for (let i = 0; i < numeriRandom.length; i++) {
 
+        // Creare i div con il numero corrispondente all'interno e la grandezza in base ai lati
+        const colString = `<div class='col-4' style='width: calc(100% / 5)'> ${numeriRandom[i]} </div>`
+        console.log(colString)
+    
+        //Aggiungere all'html il div
+        griglia.innerHTML += colString
+    }
+
+    setTimeout(function() {
+        const cols = document.querySelectorAll('.col-4');
+        cols.forEach(col => {
+            col.style.display = 'none';
+        });
+    }, sec)
+
+    // Creare SetTimeout per richiamare i prompt dopo 3 secondi
+    setTimeout(function() {
+        let randomNumbersPc = numbersToRemeber();
+    
+        const numeriCombacianti = []
+        let won = false
+    
+        for (let i = 0; i < numeriRandom.length; i++) {
+            for ( let j = 0; j < randomNumbersPc.length; j++) {
+                if ( numeriRandom[i] === randomNumbersPc[j] && !numeriCombacianti.includes(numeriRandom[i])) {
+                    numeriCombacianti.push(numeriRandom[i])
+                    won = true
+                } 
+            }
+        }
+        if (won) {
+            console.log('Hai azzeccato: ' + numeriCombacianti)
+            console.log('Hai azzeccato: ' + numeriCombacianti.length + ' numeri')
+        } else {
+            console.log('Hai perso!')
+        }
+    
+    }, sec * 2);
+
+    
 })
+
+
 
 
 //////////////// FUNZIONI //////////////////////
 
-// Creare SetTimeout per richiamare i prompt dopo 3 secondi 
-setTimeout(function() {
-    let randomNumbersPc = numbersToRemeber();
-
-    const numeriCombacianti = []
-
-    let won = false
-
-    for (let i = 0; i < numeriRandom.length; i++) {
-        for ( let j = 0; j < randomNumbersPc.length; j++) {
-            if ( numeriRandom[i] === randomNumbersPc[j]) {
-                numeriCombacianti.push(numeriRandom[i])
-                won = true
-            } 
+function isRandomNumber() {
+    let numeriRandom = [];
+    while (numeriRandom.length < 5) {
+        let numero = Math.floor(Math.random() * 100) + 1;
+        if (!numeriRandom.includes(numero)) {
+            numeriRandom.push(numero);
         }
     }
-    if (won) {
-        console.log('Hai azzeccato: ' + numeriCombacianti)
-        console.log('Hai azzeccato: ' + numeriCombacianti.length + ' numeri')
-    } else {
-        console.log('Hai perso!')
-    }
-
-}, sec);
-
-function isRandomNumber() {
-    while (numeriRandom.length < 5) {
-      let numero = Math.floor(Math.random() * 100) + 1;
-      if (!numeriRandom.includes(numero)) {
-        numeriRandom.push(numero);
-      }
-    }
-    return numeriRandom
+    return numeriRandom;
 }
 
 function numbersToRemeber() {
@@ -72,5 +90,5 @@ function numbersToRemeber() {
 }
 
 function isAvailableTime() {
-    return 3 * 1000;
+    return 5 * 1000;
 }
