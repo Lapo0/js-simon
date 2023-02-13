8216ddc9ab4f3cf5697ce82c01e89dacb4892cff
Lapo0 <lapo.gini.01@gmail.com>
2023-02-13 14:22:50 +0100

    FEAT ciclo for && while for promt

--- js/app.js
+++ js/app.js
@@ -18,3 +18,20 @@ isRandomNumber()
 
 // Creare alert per mostrare numeri
 alert('Ricorda questi numeri: ' + numeriRandom)
+
+// Assegnare SetTimeout per richiamare la funzione
+setTimeout(isRememberNumber, 3000)
+
+// Creare funzione per inserire nel promt i numeri
+function isRememberNumber() {
+    for (let i = 0; i < 5; i++) {
+        let numeroDaRicordare;
+        while (isNaN(numeroDaRicordare)) {
+            numeroDaRicordare = parseInt(prompt('Scrivi i numeri che ricordi'));
+            if (isNaN(numeroDaRicordare)) {
+                alert('Inserisci un numero valido');
+            }
+        }
+        console.log(numeroDaRicordare);
+    }
+}
